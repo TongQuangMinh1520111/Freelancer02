d5db4723062c812d1011ef339ff2ca617c1791d6
MINH_VN <tqminh@b-create.jp>
2022-10-10 17:17:48 +0700

    fix

--- html/js/top.js
+++ html/js/top.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
   });
   $(".related_slider").slick({
     slidesToShow: 3,
-    slidesToScroll:1,
+    slidesToScroll: 1,
     infinite: true,
     centerMode: false,
     touchMove: true,
@@ -45,9 +45,9 @@ $(document).ready(function () {
     autoplay: false,
     autoplaySpeed: 3000,
   });
-
+  $("#footer .footer_box01 .card").matchHeight();
+  $("#footer .footer_box01 .f_list01 dl").matchHeight();
   if ($(window).width() > 768) {
     $("#h_box04 .product-list .item img").matchHeight();
-    $("#footer .footer_box01 .card").matchHeight();
   }
 });
