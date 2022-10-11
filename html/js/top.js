f7a58cf47e4761bdc5aa10f2a0265315c4c74a56
MINH_VN <tqminh@b-create.jp>
2022-10-11 16:54:00 +0700

    update

--- html/js/top.js
+++ html/js/top.js
@@ -18,11 +18,20 @@ $(document).ready(function () {
   });
   $(".product_slider").slick({
     slidesToShow: 3,
+    slidesToScroll: 1,
     infinite: true,
     touchMove: true,
     pauseOnHover: false,
     autoplay: false,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   });
   $(".banner_slider").slick({
     slidesToShow: 1,
@@ -44,7 +53,29 @@ $(document).ready(function () {
     pauseOnHover: false,
     autoplay: false,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 491,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   });
+
+  if ($(window).width() < 1025) {
+    $(".news_slider").slick({
+      slidesToShow: 1,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      touchMove: true,
+      pauseOnHover: false,
+      autoplay: false,
+      autoplaySpeed: 3000,
+    });
+  }
+
   $("#footer .footer_box01 .card").matchHeight();
   $("#footer .footer_box01 .f_list01 dl").matchHeight();
   if ($(window).width() > 768) {
